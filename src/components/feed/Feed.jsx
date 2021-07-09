98a3bdad8464072fbc3a3704422e9c91a8d2fc02
max <777max8@gmail.com>
2021-07-09 19:45:35 +0300

    fetch like and dislike

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,19 +4,22 @@ import Post from "../post/Post";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 export default function Feed({ username }) {
   const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const { data } = username
         ? await axios.get("/post/profile/" + username)
-        : await axios.get("/post/timeline/60e1931b54f6ded7caa086d6");
+        : await axios.get("/post/timeline/" + user._id);
       setPosts(data);
     };
     fetchPosts();
-  }, []);
+  }, [username,user._id]);
 
   return (
     <div className="feed">
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,17 +4,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({ post }) {
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user: currentUser } = useContext(AuthContext);
 
   const likeHandler = () => {
+    try {
+      axios.put("/post/" + post._id + "/like", { userId: currentUser._id });
+    } catch (error) {
+      console.log(`error====>`, error.message);
+    }
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
   };
+  useEffect(() => {
+    setIsLiked(post.likes.includes(currentUser._id));
+  }, [currentUser._id, post.likes]);
+
   useEffect(() => {
     const fetchUser = async () => {
       const { data } = await axios.get(`/users?userId=${post.userId}`);
@@ -29,7 +41,11 @@ export default function Post({ post }) {
           <div className="postTopLeft">
             <Link to={`profile/${user.username}`}>
               <img
-                src={user.profilePicture || PF + "person/noAvatar.png"}
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
                 alt=""
                 className="postProfileImg"
               />
